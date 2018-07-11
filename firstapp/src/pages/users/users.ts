f835a22104f3cf74ea8f77fd32a7eecd34c2194f
vishalnanania <vishal.nanania@gmail.com>
2018-07-11 16:44:51 -0400

    lifecycle hooks - ionViewCanEnter, IonViewDidLoad, ionViewWillEnter, ionViewDidEnter, ionViewcanLeave, ionViewWillLeave, ionViewDidLeave, ionViewWillUnload.

--- firstapp/src/pages/users/users.ts
+++ firstapp/src/pages/users/users.ts
@@ -20,8 +20,42 @@ export class UsersPage {
   }
 
   ionViewCanEnter() : boolean | Promise<boolean> {
-    console.log('ionViewCanEnter UsersPage');
+    console.log('ionViewCanEnter for authguard for entering the page.');
     return Math.random() < 0.5; 
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad started laoding. onces its cahched it will not come to this method.');
+  }
+
+  ionViewWillEnter() {
+    console.log('ionViewWillEnter ready to enter the page.');
+  }
+
+  ionViewDidEnter() {
+    console.log('ionViewDidEnter entered the page.');
+  }
+
+  ionViewCanLeave() : boolean | Promise<any> {
+    console.log('ionViewCanLeave for authguard for leaving the page.');
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve()
+      }, 1000);
+    });
+    return promise;
+  }
+
+  ionViewWillLeave() {
+    console.log('ionViewWillLeave ready to leave the page.');
+  }
+
+  ionViewDidLeave() {
+    console.log('ionViewDidLeave left the page.');
+  }
+
+  ionViewWillUnload() {
+    console.log('ionViewWillUnload page is distroyed. this method will execute when page is removed from stack.');
   }
 
   onLoadUser(name: string){
