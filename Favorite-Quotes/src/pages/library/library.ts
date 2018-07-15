f4dca6f55f2e6b9cc8e68249942d61074a86e8fe
vishalnanania <vishal.nanania@gmail.com>
2018-07-15 16:52:34 -0400

    passing data using navPush and navParams.

--- Favorite-Quotes/src/pages/library/library.ts
+++ Favorite-Quotes/src/pages/library/library.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Quote } from '../../data/quote.interface';
 import quotes from '../../data/quotes';
+import { QuotesPage } from '../quotes/quotes';
 
 @IonicPage()
 @Component({
@@ -9,6 +10,7 @@ import quotes from '../../data/quotes';
   templateUrl: 'library.html',
 })
 export class LibraryPage {
+  quotesPage = QuotesPage;
   quotesCollection: {category: string, quotes: Quote[], icon: string}[]; 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
--- Favorite-Quotes/src/pages/quotes/quotes.ts
+++ Favorite-Quotes/src/pages/quotes/quotes.ts
@@ -1,25 +1,19 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the QuotesPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-quotes',
   templateUrl: 'quotes.html',
 })
 export class QuotesPage {
+  quoteGroup: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad QuotesPage');
+  ngOnInit() {
+    this.quoteGroup = this.navParams.data;
   }
 
 }
