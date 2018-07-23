a517ded67df9f2e84d0f1788f8d231773fdf5c85
vishalnanania <vishal.nanania@gmail.com>
2018-07-22 23:28:34 -0400

    adding alert to application.

--- Favorite-Quotes/src/pages/quotes/quotes.ts
+++ Favorite-Quotes/src/pages/quotes/quotes.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { Quote } from '../../data/quote.interface';
 
 @IonicPage()
 @Component({
@@ -9,11 +10,34 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class QuotesPage {
   quoteGroup: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {
   }
 
   ngOnInit() {
     this.quoteGroup = this.navParams.data;
   }
 
+  onAddToFavorite(selectedQuote: Quote) {
+    const alert = this.alertCtrl.create({
+      title: 'Add Quote',
+      subTitle: 'Are you sure?',
+      message: 'Are you sure you want to add the quote?',
+      buttons: [
+        {
+          text: 'Yes, go ahead.',
+          handler: () => {
+            console.log('ok');
+          }
+        },
+        {
+          text: 'No, i changed my mind!',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancelled');
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
 }
